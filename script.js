5e2939e1adb156cff6653bcfa81c661d57665a97
Alyssa harper <alyssaharper@Alyssas-MacBook-Air.local>
2021-07-05 13:45:00 -0400

    added Jquery to HTML. updated functions in JS

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
 var searchButtonEl = $('#searchButton');
+var searchRequestEl = $('#search');
 
-function fetchAPI() {
+function getLocation(city) {
     var APIKey = "e2343ed8866af33baef64fd64ab7247e";
-
-    var city;
 
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
     fetch(queryURL)
+    .then(function (response) {
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function (current) {
+                console.log(current);
+
+            })
+        }
+    })
+    
 };
 
 searchButtonEl.on("submit", function(event) {
     event.preventDefault();
-    var location = event.target.search.value;
-    getLocation(location);
-})
+    var city = searchRequestEl.val().trim();
+    if (city) {
+        getLocation(city);
+    }
+});
 
-function getLocation(location) {
-    fetchAPI(location);
-}
+
 
