ac0d07bdb63573a15309b1f824a421568e6a3775
Alyssa harper <alyssaharper@Alyssas-MacBook-Air.local>
2021-07-06 17:48:19 -0400

    fixed broken function for submitting cities

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var searchButtonEl = $('#searchButton');
-var searchRequestEl = $('#search');
+var searchButtonEl = document.querySelector('#searchButton');
+var searchRequestEl = document.querySelector('#search');
+var currentWeatherEl = document.querySelector('#currentWeather');
 
-function getLocation(city) {
+function currentLocation(city) {
     var APIKey = "e2343ed8866af33baef64fd64ab7247e";
 
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
@@ -10,22 +11,44 @@ function getLocation(city) {
     .then(function (response) {
         if (response.ok) {
             console.log(response);
-            response.json().then(function (current) {
-                console.log(current);
+            response.json().then(function (data) {
+                console.log(data);
 
-            })
+            });
+        } else {
+            alert('Error: ' + response.statusText);
         }
-    })
-    
+    });
+
 };
 
-searchButtonEl.on("submit", function(event) {
+function clickHandler(event) {
+    console.log('Button Clicked');
     event.preventDefault();
-    var city = searchRequestEl.val().trim();
+
+    var city = searchRequestEl.value.trim();
+
+    console.log("city")
+    console.log(city)
+
     if (city) {
-        getLocation(city);
+        currentLocation(city);
+
+        currentWeatherEl.textContent = '';
+        searchRequestEl.value = '';
+    } else {
+        alert('Please enter a city');
     }
-});
+
+
+
+ }
+
+searchButtonEl.addEventListener('click', clickHandler);
+
+
+// searchButtonEl.addEventListener("submit", formSubmitHandler);
+// console.log(currentLocation("London"));
 
 
 
